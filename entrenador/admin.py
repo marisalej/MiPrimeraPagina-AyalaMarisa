1c9139bd87f86f9bd98525fc6a6139b8fc73c18e
Marisa Ayala <marisa.ayala118@gmail.com>
2026-01-19 11:35:26 -0300

    Subo Admin TP Final

--- entrenador/admin.py
+++ entrenador/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from entrenador.models import *
 
+
 @admin.register(Entrenador)
 class Entrenador(admin.ModelAdmin):
-    # columnas visibles en la lista del modelo
     list_display = ("nombre", "apellido", "especialidad", "telefono")
-    # columnas con enlaces clickeables para entrar en el detalle
-    list_display_links = ("nombre",)	#con coma al final xq es una tupla
-    # campos por los que se pueden buscar
+    list_display_links = ("nombre",)
     search_fields = ("apellido", "nombre")
-    # filtros laterales
     list_filter = ("especialidad",)
-    # orden por defecto
     ordering = ("apellido", "nombre")
-    
--- socio/admin.py
+++ socio/admin.py
@@ -4,15 +4,7 @@ from socio.models import *
 
 @admin.register(Socio)
 class Socio(admin.ModelAdmin):
-    # columnas visibles en la lista del modelo
     list_display = ("nombre", "apellido", "dni", "email", "fecha_alta")
-    # columnas con enlaces clickeables para entrar en el detalle
-    list_display_links = ("nombre",)	#con coma al final xq es una tupla
-    # campos por los que se pueden buscar
+    list_display_links = ("nombre",)
     search_fields = ("apellido", "nombre", "dni")
-    # filtros laterales
-    #list_filter = ("fecha_de_creacion",)
-    # orden por defecto
     ordering = ("apellido", "nombre")
-    # campos de solo lectura
-    #readonly_fields = ("fecha_de_creacion",)
